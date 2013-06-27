1fb0d3e945f7b63559ffb2dd766d7845fc827126
Trond Norbye <trond.norbye@gmail.com>
2013-06-27 07:39:46 +0200

    updates

--- create.bat
+++ create.bat
@@ -7,25 +7,28 @@ git clone git://github.com/trondn/libmemcached
 git clone git://github.com/couchbase/ep-engine
 
 cd tlm
-git pull ssh://trond@review.couchbase.org:29418/tlm refs/changes/43/26543/18
+git fetch ssh://trond@review.couchbase.org:29418/tlm refs/changes/43/27143/2 && git cherry-pick FETCH_HEAD
+git fetch ssh://trond@review.couchbase.org:29418/tlm refs/changes/43/26543/22 && git cherry-pick FETCH_HEAD
 cd ..
 cd libvbucket
-git pull ssh://trond@review.couchbase.org:29418/libvbucket refs/changes/96/26096/9
+git fetch ssh://trond@review.couchbase.org:29418/libvbucket refs/changes/96/26096/10 && git cherry-pick FETCH_HEAD
 cd ..
 cd memcached
-git pull ssh://trond@review.couchbase.org:29418/memcached refs/changes/13/26113/24
+git fetch ssh://trond@review.couchbase.org:29418/memcached refs/changes/13/26113/28 && git cherry-pick FETCH_HEAD
 cd ..
 cd libconflate
-git pull ssh://trond@review.couchbase.org:29418/libconflate refs/changes/29/26929/2
+git fetch ssh://trond@review.couchbase.org:29418/libconflate refs/changes/29/26929/3 && git cherry-pick FETCH_HEAD
 cd ..
 cd couchstore
-git pull ssh://trond@review.couchbase.org:29418/couchstore refs/changes/61/26861/10
+git fetch ssh://trond@review.couchbase.org:29418/couchstore refs/changes/61/26861/11 && git cherry-pick FETCH_HEAD
 cd ..
 cd libmemcached
-git checkout -b cmake origin/cmake
+git remote add trond git://github.com/trondn/libmemcached
+git remote update
+git rebase trond/cmake
 cd ..
 cd ep-engine
-git pull ssh://trond@review.couchbase.org:29418/ep-engine refs/changes/96/26996/7
+git fetch ssh://trond@review.couchbase.org:29418/ep-engine refs/changes/96/26996/8 && git cherry-pick FETCH_HEAD
 cd ..
 
 copy tlm\CMakeLists.txt .
--- update-repo.sh
+++ update-repo.sh
@@ -1,19 +1,20 @@
 #! /bin/bash
 
 cd tlm
-git fetch ssh://trond@review.couchbase.org:29418/tlm refs/changes/43/26543/18 && git cherry-pick FETCH_HEAD
+git fetch ssh://trond@review.couchbase.org:29418/tlm refs/changes/43/27143/2 && git cherry-pick FETCH_HEAD
+git fetch ssh://trond@review.couchbase.org:29418/tlm refs/changes/43/26543/22 && git cherry-pick FETCH_HEAD
 cd ..
 cd libvbucket
-git fetch ssh://trond@review.couchbase.org:29418/libvbucket refs/changes/96/26096/9 && git cherry-pick FETCH_HEAD
+git fetch ssh://trond@review.couchbase.org:29418/libvbucket refs/changes/96/26096/10 && git cherry-pick FETCH_HEAD
 cd ..
 cd memcached
-git fetch ssh://trond@review.couchbase.org:29418/memcached refs/changes/13/26113/24 && git cherry-pick FETCH_HEAD
+git fetch ssh://trond@review.couchbase.org:29418/memcached refs/changes/13/26113/28 && git cherry-pick FETCH_HEAD
 cd ..
 cd libconflate
-git fetch ssh://trond@review.couchbase.org:29418/libconflate refs/changes/29/26929/2 && git cherry-pick FETCH_HEAD
+git fetch ssh://trond@review.couchbase.org:29418/libconflate refs/changes/29/26929/3 && git cherry-pick FETCH_HEAD
 cd ..
 cd couchstore
-git fetch ssh://trond@review.couchbase.org:29418/couchstore refs/changes/61/26861/10 && git cherry-pick FETCH_HEAD
+git fetch ssh://trond@review.couchbase.org:29418/couchstore refs/changes/61/26861/11 && git cherry-pick FETCH_HEAD
 cd ..
 cd libmemcached
 git remote add trond git://github.com/trondn/libmemcached
@@ -21,6 +22,6 @@ git remote update
 git rebase trond/cmake
 cd ..
 cd ep-engine
-git fetch ssh://trond@review.couchbase.org:29418/ep-engine refs/changes/96/26996/7 && git cherry-pick FETCH_HEAD
+git fetch ssh://trond@review.couchbase.org:29418/ep-engine refs/changes/96/26996/8 && git cherry-pick FETCH_HEAD
 cd ..
 cp tlm/CMakeLists.txt .
