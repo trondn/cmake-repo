536300d6955d9c2627e502742f6eec37e01eeda5
Trond Norbye <trond.norbye@gmail.com>
2013-06-27 11:11:01 +0200

    Fixup variables

--- update-repo.sh
+++ update-repo.sh
@@ -2,13 +2,13 @@
 
 cd tlm
 git fetch ssh://trond@review.couchbase.org:29418/tlm refs/changes/43/27143/2 && git cherry-pick FETCH_HEAD
-git fetch ssh://trond@review.couchbase.org:29418/tlm refs/changes/43/26543/24 && git cherry-pick FETCH_HEAD
+git fetch ssh://trond@review.couchbase.org:29418/tlm refs/changes/43/26543/25 && git cherry-pick FETCH_HEAD
 cd ..
 cd libvbucket
 git fetch ssh://trond@review.couchbase.org:29418/libvbucket refs/changes/96/26096/10 && git cherry-pick FETCH_HEAD
 cd ..
 cd memcached
-git fetch ssh://trond@review.couchbase.org:29418/memcached refs/changes/13/26113/28 && git cherry-pick FETCH_HEAD
+git fetch ssh://trond@review.couchbase.org:29418/memcached refs/changes/13/26113/29 && git cherry-pick FETCH_HEAD
 cd ..
 cd libconflate
 git fetch ssh://trond@review.couchbase.org:29418/libconflate refs/changes/29/26929/3 && git cherry-pick FETCH_HEAD
